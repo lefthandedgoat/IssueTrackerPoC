f11f8a26d725f404d0647e1abb0a7acd29420fe2
Chris Holt <lefthandedgoat@gmail.com>
2014-09-21 12:17:37 -0500

    Added basics of ui tests

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,8 +1,19 @@
 #dont hate!
 
-task :default => [:migrate]
+task :default => [:build, :one]
+
+task :build do
+  sh 'C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe IssueTracker.sln'  
+end
 
 task :migrate do
-  sh 'C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe IssueTracker.sln'  
   sh 'migrations\bin\debug\migrations.exe'
+end
+
+task :one do
+  sh 'tests\bin\debug\tests.exe ui one'
+end
+
+task :many do
+  sh 'tests\bin\debug\tests.exe ui many'
 end
